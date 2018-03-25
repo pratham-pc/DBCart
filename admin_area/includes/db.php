df68afcdf1610a60efc48d8fa22bbe628c7fa367
Prathamesh Chavan <pc44800@gmail.com>
2018-03-25 18:35:23 +0530

    ADdded changes made by vivek

--- admin_area/includes/db.php
+++ admin_area/includes/db.php
@@ -1,5 +1,5 @@
 <?php
-  $con = mysqli_connect("localhost","root","","dbcart");
+  $con = mysqli_connect("localhost","prathampc","qwerTY123$","dbcart");
   if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: ".mysqli_connect_error();
   }
--- functions/functions.php
+++ functions/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","root", "","dbcart");
+  $con = mysqli_connect("localhost","prathampc","qwerTY123$","dbcart");
 if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: ".mysqli_connect_error();
 }
